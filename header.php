c8ccfc30d708015bea1e5b2922dd387488c0f156
Tomiiwo Fakinlede <ftomiiwo@gmail.com>
2019-04-19 18:30:50 +0100

    Made more changes to header.php, effected in other pages

--- header.php
+++ header.php
@@ -1,3 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Unique Oceans :: Driven by Satisfaction</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="We are an indigenous management consulting firm that serves a mix of industries and sectors – Manufactur- ing, Construction, Oil & Gas – Up- stream , Downstream ( Production & Services). We help our clients make substantial and lasting im- provements to their performance and realize their most important goals. With relevant industry expe- rience, we are poised to deliver unique value for every project.
+	Uniqueocean is designed to operate as an inclusive organization that is open to global part- nership with aligned set of values, including a deep commitment to diversity.
+	We are take a consistent approach to engagement and skills development of our people irre- spective of their background just so we can deliver appropriate team mix, with the right experience and expertise, to every client, anywhere in the world.
+	Our team is composed of certified and experienced professionals in the field of Quality, Health & Safety, Environment, Humanities as well as Social workers.
+
+	A Learning Organization
+
+	We are dedicated to continuous skills development, to ensure appropriate understanding of our client requirements and context of operations. This is our niche to deliver bespoke services on every project with each client.
+	We seek to invest annually in knowledge development, learning and capability building. We study markets, trends, and emerging best practices, in every industry and region, locally and globally.
+	Our investment in knowledge also helps advance the practice of management.
+
+	Addicted to Improvement
+
+	We recognize that our clients’ needs are constantly changing, so we continually seek new and better ways to serve them. We ensure that our team are constantly developing new capabilities and we also attract new talent into the team.
+	We provide service at every level of our client organization whether at the operational level or mid-level management and strategically at the Top Management levels. We partner with clients to put recommendations into practice and work directly with them over the long-term, to help develop workforce skills, drive operational improvement, and apply new working methods.
+	No matter the challenge, we focus on delivering practical and enduring results, and equip- ping our clients with tools to improve and lead in the market space.
+
+
+	We are an indigenous management consulting firm that serves a mix of industries and sectors – Manufactur- ing, Construction, Oil & Gas – Up- stream , Downstream ( Production & Services). We help our clients make substantial and lasting im- provements to their performance and realize their most important goals. With relevant industry expe- rience, we are poised to deliver unique value for every project.
+
+	CONSULTING
+    Development And Implementation Of Management Systems – Quality, Occupational Health & Safety, Environmental Management Systems
+    Strategy Development
+    Process identification and Systems Re-Engineering
+    Organizational Culture
+    Business Risk Analysis
+	AUDIT SERVICES
+    Gap Analysis
+    Pre– Assessment Audits (Pre– Certification)
+    Supplier Audits
+    Internal Audits
+    Project Audits
+	PROJECT SUPPORT
+    Inspection Services (Welding, Coating, Storage, Fabrication)
+    Coordination Project Quality Assurance and Control
+    Preparation and Implementation of Inspection Test Plans
+    Project Documentation and Reviews
+    Client Representation in Project Quality Management
+	TRAINING SERVICES
+    PROVISION OF ACCREDITED ISO MANAGEMENT SYSTEMS STANDARDS TRAINING (IRCA & PECB)
+    ISO 9001:2015 (Foundation, Implementer, Internal & Lead Auditors) Courses
+    ISO 14001:2015 (Foundation, Implementer, Internal & Lead Auditor) Courses
+    ISO 45001:2015 (Foundation, Implementer, Internal & Lead Auditor) Courses
+    BESPOKE QUALITY TRAININGS
+    Document Control
+    Use of Quality Tools
+    Root Cause Analysis & Incident Investigation
+    INTEGRATED MANAGEMENT SYSTEM
+	" />
+
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script> 
+	<!-- css files -->
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' /><!-- bootstrap css -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' /><!-- custom css -->
+    <link href="css/font-awesome.min.css" rel="stylesheet"><!-- fontawesome css -->
+	<!-- //css files -->
+	<!-- google fonts -->
+	<link href="//fonts.googleapis.com/css?family=Cabin:400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+	<!-- //google fonts -->
+	<link rel="apple-touch-icon" sizes="57x57" href="images/fav/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="images/fav/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/fav/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="images/fav/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/fav/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="images/fav/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="images/fav/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="images/fav/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="images/fav/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="images/fav/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="images/fav/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="images/fav/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/fav/favicon-16x16.png">
+	<link rel="manifest" href="images/fav/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="images/fav/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+</head>
+<body>
 <!-- //header -->
 <header class="py-4">
 	<div class="container">
